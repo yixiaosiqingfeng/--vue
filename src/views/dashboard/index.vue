9955b461022dac12463d69d47c63f54e2d2dae07
cvacva <769978880@qq.com>
2019-07-08 15:41:49 +0800

    test 视频上传

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="dashboard-container">
     ewdwed
+    wefwefwef
+    <div>rfwefewwef</div>
   </div>
 </template>
 
 <script>
 
-  export default {
-    name: 'Dashboard',
-    data() {
-      return {
-        currentRole: 'adminDashboard'
-      }
+export default {
+  name: 'Dashboard',
+  data() {
+    return {
+      currentRole: 'adminDashboard'
     }
   }
+}
 </script>
 <style scoped>
   .dashboard-container{
