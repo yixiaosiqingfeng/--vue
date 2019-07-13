2ba40a68016e46be09f285a503d6f2dfa488e444
cvacva <769978880@qq.com>
2019-07-13 20:28:44 +0800

    话题完成

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,32 +9,57 @@ const service = axios.create({
   // baseURL: 'http://192.168.0.19:8084', // 宇驰
   // baseURL: 'http://192.168.0.14:8084', // 帅哥
   // baseURL: 'http://192.168.0.18:8084', // 胡超
-  // baseURL: 'http://192.168.0.17:8084', // 展鹏
+  //  baseURL: 'http://192.168.0.17:8084', // 展鹏
   baseURL: 'http://192.168.0.254:8084', // 服务器
   timeout: 5000 // request timeout
 })
+
+// request interceptor
 service.interceptors.request.use(
   config => {
+    // do something before request is sent
+
     if (store.getters.token) {
+      // let each request carry token
+      // ['X-Token'] is a custom headers key
+      // please modify it according to the actual situation
       config.headers['token'] = getToken()
     }
     return config
   },
   error => {
+    // do something with request error
     console.log(error) // for debug
     return Promise.reject(error)
   }
 )
+
+// response interceptor
 service.interceptors.response.use(
+  /**
+   * If you want to get http information such as headers or status
+   * Please return  response => response
+  */
+
+  /**
+   * Determine the request status by custom code
+   * Here is just an example
+   * You can also judge the status by HTTP Status Code
+   */
   response => {
     const res = response.data
+
+    // if the custom code is not 20000, it is judged as an error.
     if (res.errorCode !== 0) {
       Message({
         message: res.message || 'Error',
         type: 'error',
         duration: 5 * 1000
       })
+
+      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+        // to re-login
         MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
           confirmButtonText: 'Re-Login',
           cancelButtonText: 'Cancel',
@@ -60,4 +85,5 @@ service.interceptors.response.use(
     return Promise.reject(error)
   }
 )
+
 export default service
